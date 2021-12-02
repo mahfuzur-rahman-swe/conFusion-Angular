211bac4f17a6cdf46c62cf9fe7e6bca0c4604648
Mahfuzur Rahman <63579852+Mahfuz00741@users.noreply.github.com>
2021-12-02 09:30:45 +0600

    conFusion assignment 1 done

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,11 +12,13 @@ import { MenuComponent } from './menu/menu.component';
 import {MatListModule} from "@angular/material/list";
 import {MatGridListModule} from "@angular/material/grid-list";
 import {MatCardModule} from "@angular/material/card";
+import { DishdetailComponent } from './dishdetail/dishdetail.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    MenuComponent
+    MenuComponent,
+    DishdetailComponent
   ],
     imports: [
         BrowserModule,
