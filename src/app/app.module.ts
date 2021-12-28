4cd7a80ea527ef1df121d2859c17b388cdcb934a
Mahfuzur Rahman <mahfuz00741@gmail.com>
2021-12-28 17:55:45 +0600

    Header and Footer add

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,12 +14,16 @@ import {MatGridListModule} from "@angular/material/grid-list";
 import {MatCardModule} from "@angular/material/card";
 import { DishdetailComponent } from './dishdetail/dishdetail.component';
 import {DishService} from "./services/dish.service";
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     MenuComponent,
-    DishdetailComponent
+    DishdetailComponent,
+    HeaderComponent,
+    FooterComponent
   ],
     imports: [
         BrowserModule,
