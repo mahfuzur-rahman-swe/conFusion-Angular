50ff64aa0e05a140289a95c160a2f0a3c64a3062
Mahfuzur Rahman <mahfuz00741@gmail.com>
2021-12-29 17:20:55 +0600

    Single Page Applications Part 1

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import {HomeComponent} from "./home/home.component";
 import {MenuComponent} from "./menu/menu.component";
+import {ContactComponent} from "./contact/contact.component";
 
 const routes: Routes = [
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
   { path: 'home', component: HomeComponent },
   { path: 'menu',     component: MenuComponent },
-  { path: '', redirectTo: '/home', pathMatch: 'full' }
+  { path: 'contactus', component: ContactComponent },
 ];
 
 @NgModule({
